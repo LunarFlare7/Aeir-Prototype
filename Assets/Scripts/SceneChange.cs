d2e0fc52ec086583f0fd634001fbbc4518b576b1
Samuel <samuel@chokron.com>
2024-05-08 14:41:09 -0400

    Added intro scene and animation

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public string SceneName;
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
